2a7713614fbbf2c7232717aa89b7a16c4dc71ede
Dennyliu <dennyliu@redseaent.com>
2023-03-15 17:24:23 +0800

    first commit

---
+++ app/code/Haventop/HelloWorld/Block/Index.php
@@ -0,0 +1,6 @@
+<?php
+namespace Haventop\HelloWorld\Block;
+class Index extends \Magento\Framework\View\Element\Template
+{
+
+}
---
+++ app/code/Pfay/Contacts/Controller/Frontend/Index.php
@@ -0,0 +1,9 @@
+<?php
+namespace Pfay\Contacts\Controller\Frontend;
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        die('Pfay contacts frontend package index');
+    }
+}
